d094880f2e60cfe8287c88f5f3314aa90be9c1db
Thiago Santos <91476713+Thmsantos@users.noreply.github.com>
2024-05-14 18:47:58 -0300

    Update 1021.py

--- interFatecs/1021.py
+++ interFatecs/1021.py
@@ -28,45 +28,42 @@
 # moeda(s) de R$ 0.05
 # moeda(s) de R$ 0.01
 
-n = float(input())
-
+A=float(input())
+N=A
+a=N/100
+b=N%100
+c=b/50
+d=b%50
+e=d/20
+f=d%20
+g=f/10
+h=f%10
+i=h/5
+j=h%5
+k=j/2
+l=j%2
+E=A*100
+B=int(E)
+m=B%100
+n=m/50
+o=m%50
+p=o/25
+q=o%25
+r=q/10
+s=q%10
+t=s/5
+u=s%5
 print("NOTAS:")
-print("%d nota(s) de R$ 100.00" % (n/100))
-
-n = n%100
-print("%d nota(s) de R$ 50.00" % (n/50))
-
-n = n%50
-print("%d nota(s) de R$ 20.00" % (n/20))
-
-n = n%20
-print("%d nota(s) de R$ 10.00" % (n/10))
-
-n = n%10
-print("%d nota(s) de R$ 5.00" % (n/5))
-
-n = n%5
-print("%d nota(s) de R$ 2.00" % (n/2))
-
+print("%d nota(s) de R$ 100.00"%a)
+print("%d nota(s) de R$ 50.00"%c)
+print("%d nota(s) de R$ 20.00"%e)
+print("%d nota(s) de R$ 10.00"%g)
+print("%d nota(s) de R$ 5.00"%i)
+print("%d nota(s) de R$ 2.00"%k)
 print("MOEDAS:")
-
-n = n%2
-print("%d moeda(s) de R$ 1.00" % (n/1))
-
-n = n%1
-print("%d moeda(s) de R$ 0.50" % (n/0.50))
-
-n = n%0.50
-print("%d moeda(s) de R$ 0.25" % (n/0.25))
-
-n = n%0.25
-print("%d moeda(s) de R$ 0.10" % (n/0.10))
-
-n = n%0.10
-print("%d moeda(s) de R$ 0.05" % (n/0.05))
-
-n = n%0.05
-print("%d moeda(s) de R$ 0.01" % (n/0.01))
-
-
-
+print("%d moeda(s) de R$ 1.00"%l)
+print("%d moeda(s) de R$ 0.50"%n)
+print("%d moeda(s) de R$ 0.25"%p)
+print("%d moeda(s) de R$ 0.10"%r)
+print("%d moeda(s) de R$ 0.05"%t)
+print("%d moeda(s) de R$ 0.01"%u)
